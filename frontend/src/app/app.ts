8fce7607ef73102d6f20cbbcdd0639fd4b23d952
Davide Ingargiola <topavide@gmail.com>
2026-04-03 17:06:18 +0200

    Added Contact us page

--- frontend/src/app/app.ts
+++ frontend/src/app/app.ts
@@ -2,11 +2,12 @@ import { Component } from '@angular/core';
 import { HeaderComponent } from './components/header/header';
 import { HeroComponent } from './components/hero/hero';
 import { AboutComponent } from './components/about/about';
+import { ContactComponent } from './components/contact/contact';
 import { FooterComponent } from './components/footer/footer';
 
 @Component({
   selector: 'app-root',
-  imports: [HeaderComponent, HeroComponent, AboutComponent, FooterComponent],
+  imports: [HeaderComponent, HeroComponent, AboutComponent, ContactComponent, FooterComponent],
   templateUrl: './app.html',
   styleUrl: './app.scss'
 })
